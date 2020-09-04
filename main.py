1682302223ab97e7468e0154f94fdf369577c8d8
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2020-09-04 20:18:30 +0530

    Cleaned up the generated plot

--- main.py
+++ main.py
@@ -12,27 +12,28 @@ class PlotData:
         self.avg_deg = []
 
 
-nnodes = 100
+number_nodes = 1000
 data = []
 g = nx.Graph()
-g.add_nodes_from(list(range(nnodes)))
+g.add_nodes_from(list(range(number_nodes)))
 max_deg = 0
 
 
 def sample_plot():
     components = nx.connected_components(g)
     data.append(PlotData())
-    max_deg = 0
+    curr_max_deg = 0
     for component in components:
         data[-1].n_comp += 1
         data[-1].sizes.append(len(component))
         avg = 0
         for node in component:
+            # noinspection PyUnresolvedReferences
             avg += g.degree[node]
         avg /= data[-1].sizes[-1]
-        max_deg = max(max_deg, avg)
+        curr_max_deg = max(curr_max_deg, avg)
         data[-1].avg_deg.append(avg)
-    return max_deg
+    return curr_max_deg
 
 
 while not nx.is_connected(g):
@@ -46,13 +47,17 @@ x = data[-1].sizes
 y = data[-1].avg_deg
 plt.subplots_adjust(left=0.25, bottom=0.25)
 fig, ax = plt.subplots()
-plt.xlim(0, nnodes)
+plt.subplots_adjust(left=0.125, bottom=0.157, right=0.9, top=0.95, wspace=0.2, hspace=0.2)
+
+plt.xlim(0, number_nodes)
 plt.ylim(0, max_deg)
+plt.xlabel("Size of component")
+plt.ylabel("Average degree of nodes")
+
 p = plt.scatter(x, y, color=(0.2, 0.6, 0.1, 0.5))
 ax.margins(x=0)
-axcolor = 'lightgoldenrodyellow'
-axis = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-slide = Slider(axis, 'Step', 0, len(data)-1, valinit=0, valstep=1)
+axis = plt.axes([0.15, 0.02, 0.7, 0.03])
+slide = Slider(axis, '# Edges', 0, len(data)-1, valinit=0, valstep=1)
 
 
 def update(val):
