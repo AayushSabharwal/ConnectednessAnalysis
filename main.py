80720fc2e6c2f4fb396cf53f42ccd3408c45b909
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2020-09-04 21:46:12 +0530

    Formatting

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ point_colour = (0.2, 0.6, 0.1, 0.5)
 # number of nodes in the graph
 number_nodes = 1000
 
-data = []   # to store data after adding each edge
+data = []  # to store data after adding each edge
 g = nx.Graph()
 g.add_nodes_from(list(range(number_nodes)))
-max_deg = 0 # the maximum average degree of a component, used to properly set y axis limits while making plot
+max_deg = 0  # the maximum average degree of a component, used to properly set y axis limits while making plot
 
 
 def sample_plot():
@@ -29,8 +29,8 @@ def sample_plot():
     Function to sample the data of the graph, and add it to the data list
     :returns maximum average degree of any component at this step
     """
-    components = nx.connected_components(g)     # get the connected components
-    data.append(PlotData())     # add a plot data to the list
+    components = nx.connected_components(g)  # get the connected components
+    data.append(PlotData())  # add a plot data to the list
     curr_max_deg = 0
     for component in components:
         data[-1].n_comp += 1
@@ -46,11 +46,11 @@ def sample_plot():
 
 
 while not nx.is_connected(g):
-    max_deg = max(max_deg, sample_plot())   # sample the graph and update maximum average degree
-    edge = random.choices(list(g.nodes()), k=2)     # choose a random edge
-    g.add_edge(edge[0], edge[1])    # add the random edge
+    max_deg = max(max_deg, sample_plot())  # sample the graph and update maximum average degree
+    edge = random.choices(list(g.nodes()), k=2)  # choose a random edge
+    g.add_edge(edge[0], edge[1])  # add the random edge
 
-sample_plot()   # sample graph after it is connected
+sample_plot()  # sample graph after it is connected
 
 # plotting
 x = data[0].sizes
@@ -67,7 +67,7 @@ plt.ylabel("Average degree of nodes")
 p = plt.scatter(x, y, color=point_colour)
 ax.margins(x=0)
 axis = plt.axes([0.15, 0.02, 0.7, 0.03])
-slide = Slider(axis, '# Edges', 0, len(data)-1, valinit=0, valstep=1)
+slide = Slider(axis, '# Edges', 0, len(data) - 1, valinit=0, valstep=1)
 
 
 def update(val):
